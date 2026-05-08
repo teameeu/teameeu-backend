5837cda4167ce010a18615a4b39612214b372693
Minter <adf5858@naver.com>
2026-05-08 15:50:38 +0900

    feat: dto 변환 메서드 추가

--- src/main/java/com/teameau/waymore/user/dto/GradeResponse.java
+++ src/main/java/com/teameau/waymore/user/dto/GradeResponse.java
@@ -1,9 +1,20 @@
 package com.teameau.waymore.user.dto;
 
+import com.teameau.waymore.user.domain.Grade;
+
 public record GradeResponse(
         Long gradeId,
         String subject,
         Integer score,
         String grade
 ) {
+
+    public static GradeResponse from(Grade grade) {
+        return new GradeResponse(
+                grade.getGradeId(),
+                grade.getSubject(),
+                grade.getScore(),
+                grade.getGrade()
+        );
+    }
 }
